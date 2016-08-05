ef8bf7e5355c283f3b8d68b27214e589ea9a9bbf
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-05 17:32:59 +0200

    Experiment with organiztion

--- configuration.rb
+++ configuration.rb
@@ -4,15 +4,20 @@ class Configuration
   attr_accessor :endpoint
   attr_accessor :file_path
   attr_accessor :directory
+  attr_accessor :orgMode
 
-  def initialize(user, endpoint, file_path, directory)
+  def initialize(user, endpoint, file_path, directory, orgMode)
     @user = user
     @endpoint = endpoint
     @file_path = file_path
     @directory = directory
+    @directory = orgMode
   end
 
   def url()
+    if orgMode
+      return "https://api.github.com/orgs/#{user}/repos"
+    end
     return "https://api.github.com/users/#{user}/#{endpoint}"
   end
 end
--- run.rb
+++ run.rb
@@ -16,8 +16,9 @@ user = ENV['GITHUB_USER'] || "scanf"
 local_git_directory = ENV['CGIT_REPO_DIRECTORY'] || "/tmp"
 cgit_repositories_rcpath = ENV['CGIT_REPOSITORIES_FILE'] || "/etc/github_repositories"
 
-configs = [Configuration.new(user, "starred", cgit_repositories_rcpath, local_git_directory),
-           Configuration.new(user, "repos", cgit_repositories_rcpath, local_git_directory)]
+configs = [Configuration.new(user, "starred", cgit_repositories_rcpath, local_git_directory, true),
+           Configuration.new(user, "repos", cgit_repositories_rcpath, local_git_directory, false),
+           Configuration.new("swift", "", cgit_repositories_rcpath, local_git_directory, true)]
 
 configs.each do |config|
   pull_repositories_with(config)
