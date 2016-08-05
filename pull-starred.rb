3a3fcd1b122c2423c252d0339af3d2f128f8b4b4
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-05 15:42:14 +0200

    Fix errors

--- pull-starred.rb
+++ pull-starred.rb
@@ -2,6 +2,7 @@ require 'httparty'
 
 write_path = ENV['STARRED_GITHUB_REPOSITORIES'] || "/etc/starred_repositories"
 user = ENV['GITHUB_USER'] || "scanf"
+local_git_directory = ENV['GIT_REPO_DIRECTORY'] || "/tmp"
 starred_file = File.open(write_path, 'w')
 
 starred_repositories = HTTParty.get("https://api.github.com/users/#{user}/starred")
@@ -9,16 +10,16 @@ starred_repositories.each  do |repo|
   name = repo["name"]
   description = repo["description"]
   git_url = repo["git_url"]
-  local_path = "/tmp/#{name}.git" # is this right?
+  local_path = "#{local_git_directory}/#{name}.git" # is this right?
   if File.directory?(local_path)
     `cd #{local_path} && git pull --quiet --all`
   else
     `git clone --quiet #{git_url} #{local_path}`
   end
-  starred_file("repo.url=#{name}")
-  starred_file("repo.path=#{local_path}")
-  starred_file("repo.desc=#{description}")
-  starred_file("")
+  starred_file.write("repo.url=#{name}")
+  starred_file.write("repo.path=#{local_path}")
+  starred_file.write("repo.desc=#{description}")
+  starred_file.write("")
 end
 
 starred_file.close unless starred_file.nil?
