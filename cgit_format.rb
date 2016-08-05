5a3a9f02e4812000b4e933cff7b60b581cceba6b
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-05 16:48:58 +0200

    Split out cgit format generation into own file

---
+++ cgit_format.rb
@@ -0,0 +1,18 @@
+def write_cgit_format_to(config_file, repos, local_git_directory)
+  repos.each  do |repo| 
+    name = repo["name"]
+    description = repo["description"]
+    owner=repo["owner"]["login"]
+    git_url = repo["git_url"]
+    local_path = "#{local_git_directory}/#{name}.git" # is this right?
+    if File.directory?(local_path)
+      `cd #{local_path} && git pull --quiet --all &`
+    else
+      `git clone --quiet #{git_url} #{local_path} &`
+    end
+    config_file.write("\nrepo.url=#{name}\n")
+    config_file.write("repo.path=#{local_path}\n")
+    config_file.write("repo.desc=#{description}\n")
+    config_file.write("repo.owner=#{owner}\n")
+  end
+end
--- pull-starred.rb
+++ pull-starred.rb
@@ -1,37 +1,17 @@
 require 'httparty'
 require 'fileutils'
+require_relative 'cgit_format.rb'
 
-
-def write_cgit_format_to(config_file, repos)
-  local_git_directory = ENV['GIT_REPO_DIRECTORY'] || "/tmp"
-  repos.each  do |repo| 
-    name = repo["name"]
-    description = repo["description"]
-    owner=repo["owner"]["login"]
-    git_url = repo["git_url"]
-    local_path = "#{local_git_directory}/#{name}.git" # is this right?
-    if File.directory?(local_path)
-      `cd #{local_path} && git pull --quiet --all &`
-    else
-      `git clone --quiet #{git_url} #{local_path} &`
-    end
-    config_file.write("\nrepo.url=#{name}\n")
-    config_file.write("repo.path=#{local_path}\n")
-    config_file.write("repo.desc=#{description}\n")
-    config_file.write("repo.owner=#{owner}\n")
-    return # debug, don't want to wait so long
-  end
-end
-
-def pull_starred_repositories_to(write_path, user)
+def pull_starred_repositories_to(write_path, user, local_git_directory)
   tmp_path = "#{write_path}.tmp"
   starred_file = File.open(tmp_path, 'w')
   starred_repositories = HTTParty.get("https://api.github.com/users/#{user}/starred")
-  write_cgit_format_to(starred_file, starred_repositories)
+  write_cgit_format_to(starred_file, starred_repositories, local_git_directory)
   starred_file.close unless starred_file.nil?
   FileUtils.mv(tmp_path, write_path)
 end
 
+local_git_directory = ENV['GIT_REPO_DIRECTORY'] || "/tmp"
 user = ENV['GITHUB_USER'] || "scanf"
 starred_path = ENV['STARRED_GITHUB_REPOSITORIES'] || "/etc/starred_repositories"
-pull_starred_repositories(starred_path, user)
+pull_starred_repositories_to(starred_path, user, local_git_directory)
