51db5ca86a3bfb4042b9071cfca1da9f3b12846f
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-06 13:14:53 +0000

    Only open file once for each configuration

--- run.rb
+++ run.rb
@@ -3,14 +3,6 @@ require 'fileutils'
 require_relative 'cgit_format.rb'
 require_relative 'configuration.rb'
 
-def pull_repositories_with(config)
-  tmp_path = "#{config.file_path}.tmp"
-  file = File.open(tmp_path, 'w')
-  repositories = HTTParty.get(config.url())
-  write_cgit_format_to(file, repositories, config.directory)
-  file.close unless file.nil?
-  FileUtils.mv(tmp_path, config.file_path)
-end
 
 user = ENV['GITHUB_USER'] || "scanf"
 local_git_directory = ENV['CGIT_REPO_DIRECTORY'] || "/tmp"
@@ -19,6 +11,12 @@ cgit_repositories_rcpath = ENV['CGIT_REPOSITORIES_FILE'] || "/etc/github_reposit
 configs = [Configuration.new(user, "starred", cgit_repositories_rcpath, local_git_directory),
            Configuration.new(user, "repos", cgit_repositories_rcpath, local_git_directory)]
 
+
+tmp_path = "#{cgit_repositories_rcpath}.tmp"
+file = File.open(tmp_path, 'w')
 configs.each do |config|
-  pull_repositories_with(config)
+  repositories = HTTParty.get(config.url())
+  write_cgit_format_to(file, repositories, config.directory)
 end
+file.close unless file.nil?
+FileUtils.mv(tmp_path, cgit_repositories_rcpath)
