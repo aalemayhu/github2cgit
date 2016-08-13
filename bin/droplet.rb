040004290b765d43508756e076933eb0ad42e8bb
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-14 00:42:50 +0200

    Replace scripts with bin
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

---
+++ bin/droplet.rb
@@ -0,0 +1,13 @@
+require_relative "../src/app.rb"
+
+local_git_directory = "/srv/git"
+cgit_repositories_rcpath = "/etc/github_repositories"
+
+`mkdir -p #{local_git_directory}`
+`touch #{cgit_repositories_rcpath}`
+
+app = App.new("scanf", local_git_directory, cgit_repositories_rcpath)
+app.run()
+
+`sudo chown www-data:www-data #{cgit_repositories_rcpath}`
+`sudo chown -R www-data:www-data #{local_git_directory}`
---
+++ bin/local_dev.rb
@@ -0,0 +1,10 @@
+require_relative "../src/app.rb"
+
+local_git_directory = "/tmp/cgit"
+cgit_repositories_rcpath = "/tmp/cgit/github_repositories"
+
+`mkdir -p #{local_git_directory}`
+`touch #{cgit_repositories_rcpath}`
+
+app = App.new("scanf", local_git_directory, cgit_repositories_rcpath)
+app.run()
