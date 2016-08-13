c0061613cbd54daf258387060cde3e7b7cb3bc01
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-14 00:22:59 +0200

    Merge cgit and repository
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/cgit.rb
+++ src/cgit.rb
@@ -1,6 +1,30 @@
-require_relative 'repository.rb'
-
 module Cgit
+
+  class Repository
+    attr_accessor :name
+    attr_accessor :description
+    attr_accessor :owner
+    attr_accessor :git_url
+
+    def initialize(name, description, owner, git_url)
+      @name = name
+      @description = description
+      @owner = owner
+      @git_url = git_url
+    end
+
+    def local_path(local_git_directory)
+      return "#{local_git_directory}/#{owner}/#{name}.git"
+    end
+
+    def to_string(local_git_directory)
+      return "\nrepo.url=#{name}\n" \
+      "repo.path=#{local_path(local_git_directory)}\n" \
+      "repo.desc=#{description}\n" \
+      "repo.owner=#{owner}\n"
+    end
+  end
+
   def map_to_cgit_repos(github_repository_payload)
     cgit_repos = []
     github_repository_payload.each do |repo|
