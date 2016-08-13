141ecfe07d1bc0079edeac72fc97cc9245ef4284
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-14 00:16:21 +0200

    Introduce new class to simplify run script
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- bin/run.rb
+++ bin/run.rb
@@ -1,38 +1,8 @@
-require 'httparty'
-require 'fileutils'
-require_relative '../src/cgit_format.rb'
-require_relative '../src/configuration.rb'
-require_relative '../src/process.rb'
-
+require_relative "../src/app.rb"
 
 user = ENV['GITHUB_USER'] || "scanf"
 local_git_directory = ENV['CGIT_REPO_DIRECTORY'] || "/tmp"
 cgit_repositories_rcpath = ENV['CGIT_REPOSITORIES_FILE'] || "/etc/github_repositories"
 
-configs = [Configuration.new(user, "starred", cgit_repositories_rcpath, local_git_directory),
-           Configuration.new(user, "repos", cgit_repositories_rcpath, local_git_directory)]
-
-cgit_repositories = []
-configs.each do |config|
-  github_repositories = HTTParty.get(config.url())
-  cgit_repositories += map_to_cgit_repos(github_repositories)
-end
-
-cgit_repositories = cgit_repositories.uniq { |repo| repo.git_url }
-
-# Write config and clone all of the stuff
-
-tmp_path = "#{cgit_repositories_rcpath}.tmp"
-config_file = File.open(tmp_path, 'w')
-cgit_repositories.each  do |repo| 
-  local_path = "#{local_git_directory}/#{repo.owner}/#{repo.name}.git" # is this right?
-  if File.directory?(local_path)
-    runInBackground("cd #{local_path} && git remote update &")
-  else
-    runInBackground("git clone --mirror --quiet #{repo.git_url} #{local_path} &")
-  end
-  config_file.write(repo.to_string())
-end
-
-config_file.close unless config_file.nil?
-FileUtils.mv(tmp_path, cgit_repositories_rcpath)
+app = App.new(user, local_git_directory, cgit_repositories_rcpath)
+app.run()
---
+++ src/app.rb
@@ -0,0 +1,47 @@
+require 'httparty'
+require 'fileutils'
+require_relative '../src/cgit_format.rb'
+require_relative '../src/configuration.rb'
+require_relative '../src/process.rb'
+
+class App
+  attr_accessor :user
+  attr_accessor :local_git_directory
+  attr_accessor :cgit_repositories_rcpath
+
+  def initialize(user, directory, config_path)
+    @user = user
+    @local_git_directory = directory
+    @cgit_repositories_rcpath = config_path
+  end
+
+  def run
+    configs = [Configuration.new(user, "starred", cgit_repositories_rcpath, local_git_directory),
+               Configuration.new(user, "repos", cgit_repositories_rcpath, local_git_directory)]
+
+    cgit_repositories = []
+    configs.each do |config|
+      github_repositories = HTTParty.get(config.url())
+      cgit_repositories += map_to_cgit_repos(github_repositories)
+    end
+
+    cgit_repositories = cgit_repositories.uniq { |repo| repo.git_url }
+
+# Write config and clone all of the stuff
+
+    tmp_path = "#{cgit_repositories_rcpath}.tmp"
+    config_file = File.open(tmp_path, 'w')
+    cgit_repositories.each  do |repo|
+      local_path = "#{local_git_directory}/#{repo.owner}/#{repo.name}.git" # is this right?
+      if File.directory?(local_path)
+        runInBackground("cd #{local_path} && git remote update &")
+      else
+        runInBackground("git clone --mirror --quiet #{repo.git_url} #{local_path} &")
+      end
+      config_file.write(repo.to_string())
+    end
+
+    config_file.close unless config_file.nil?
+    FileUtils.mv(tmp_path, cgit_repositories_rcpath)
+  end
+end
